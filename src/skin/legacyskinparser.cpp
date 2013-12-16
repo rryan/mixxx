8f547e503cc92e4a4281ac92c11ecace58f184db
RJ Ryan <rryan@mixxx.org>
2013-12-16 15:23:10 -0500

    Remove another hack. Not sure why I put this on the splitter.

--- src/skin/legacyskinparser.cpp
+++ src/skin/legacyskinparser.cpp
@@ -435,15 +435,6 @@ QWidget* LegacySkinParser::parseSplitter(QDomElement node) {
             pSplitter->setOrientation(Qt::Horizontal);
         }
     }
-    QString backgroundPath = XmlParse::selectNodeQString(node, "Background");
-    if (backgroundPath.length() > 0) {
-        QPixmap* backgroundPixmap = WPixmapStore::getPixmapNoCache(WWidget::getPath(backgroundPath));
-        QLabel* pBackground = new QLabel(pSplitter);
-        pBackground->setPixmap(*backgroundPixmap);
-        // getPixmapNoCache allocates a new pixmap and QLabel clones it so we need
-        // to free this one.
-        delete backgroundPixmap;
-    }
 
     QDomNode childrenNode = XmlParse::selectNode(node, "Children");
 
