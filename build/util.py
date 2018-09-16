0f935e1ad761f2dcab8d51a7ceaa47ad771af3d6
RJ Skerry-Ryan <rryan@mixxx.org>
2018-09-15 17:07:43 -0700

    Move verson and packaging helper functions out of src/SConscript into build/util.py.

--- build/util.py
+++ build/util.py
@@ -4,6 +4,7 @@ import os
 import os.path
 import re
 import stat
+import time
 
 CURRENT_VCS = None
 
@@ -234,3 +235,114 @@ def write_rc_include_header(mixxx_version, vcs_revision, is_debug):
             str_list.append('#define PRERELEASE                  1\n')
 
         fo.write(''.join(str_list))
+
+
+def ubuntu_append_changelog(debian_dir,
+                            package_name, package_version,
+                            description,
+                            distro='lucid',
+                            urgency='low',
+                            author="Mixxx Buildbot <builds@mixxx.org>"):
+    now_formatted = time.strftime("%a,  %d %b %Y %H:%M:%S +0000", time.gmtime())
+    new_entry = [
+        "%s (%s) %s; urgency=%s" % (package_name, package_version, distro, urgency),
+        "",
+        description,
+        "",
+        " -- %s  %s" % (author, now_formatted),
+        "",
+    ]
+    lines = []
+    with open(os.path.join(debian_dir, 'changelog'), 'r') as changelog:
+        lines = list(changelog)
+    with open(os.path.join(debian_dir, 'changelog'), 'w') as changelog:
+        changelog.writelines(["%s\n" % x for x in new_entry])
+        changelog.writelines(lines)
+
+
+def ubuntu_cleanup():
+    os.system('rm -rf ubuntu')
+    os.mkdir('ubuntu')
+
+
+def construct_version(build, mixxx_version, branch_name, vcs_revision):
+    if branch_name.startswith('release-'):
+        branch_name = branch_name.replace('release-', '')
+
+    # Include build type in the filename.
+    build_type = 'release' if build.build_is_release else 'debug'
+
+    # New, simpler logic: mixxx version, branch name, git revision,
+    # release/build. Example: mixxx-1.12.0-master-gitXXXX-release
+    return "%s-%s-%s%s-%s" % (mixxx_version, branch_name, vcs_name,
+                              vcs_revision, build_type)
+
+
+def ubuntu_construct_version(build, mixxx_version, branch_name, vcs_revision,
+                             ubuntu_version, distro_version):
+    # The format of a Debian/Ubuntu version is:
+    #
+    #   [epoch:]upstream_version[-debian_revision]
+    #
+    # A detailed description of the valid characters and sorting order of
+    # versions can be found here:
+    # https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Version
+    #
+    # For package upgrades to work correctly, we want the following
+    # orderings on package versions:
+    #
+    # nightly build < pre-alpha < alpha < beta < rc1 < rc2 < final release
+    #
+    # The sorting rules are complicated but the key detail is: "The lexical
+    # comparison is a comparison of ASCII values modified so that all the
+    # letters sort earlier than all the non-letters and so that a tilde
+    # sorts before anything, even the end of a part."
+    #
+    # The Mixxx version stored in src/defs_version.h (the "mixxx_version"
+    # parameter to this function) is formatted like:
+    #
+    # Pre Alpha: 2.0.0-alpha-pre
+    # Alpha: 2.0.0-alpha
+    # Beta: 2.0.0-beta
+    # RC: 2.0.0-rc1
+    # Final: 2.0.0
+    #
+    # Since hyphens are a separator character between the upstream version
+    # and Debian version, we replace these with tildes.
+    #
+    # Other goals:
+    # - We would like to know the branch and commit of a package.
+    # - We would like the PPA to trump the official Debian package.
+    #
+    # The following versions are sorted from low to high order:
+    # 1.9.9
+    # 2.0.0~alpha~pre
+    # 2.0.0~alpha
+    # 2.0.0~beta~pre
+    # 2.0.0~beta
+    # 2.0.0~dfsg4 <- official Debian package version
+    # 2.0.0~rc1
+    # 2.0.0
+    # 2.0.1~alpha~pre
+    #
+    # Our official Debian packages have a ~dfsg section, so in this case an
+    # rc1 package in our PPA would trump an official Debian package
+    # (probably not what we want but not too bad since we would probably
+    # publish a "2.0.0" final to our PPA before the official Debian package
+    # is even released.
+    #
+    # Note in the above sorted list that if the branch name were included
+    # after the mixxx_version, 2.0.0~master would sort earlier than
+    # 2.0.0~rc1~master!  To prevent branch and revision tags from
+    # interfering with package ordering we include them in the
+    # debian_revision portion of the version. This ensures they are only
+    # used for sorting if the upstream version of two packages is identical.
+    upstream_version = mixxx_version.replace('-', '~')
+    assert '_' not in upstream_version
+
+    # Strip underscores and dashes in the branch name.
+    branch_name = branch_name.strip('_-')
+    assert branch_name and branch_name != '(no branch)'
+
+    return "%s-%s~%s~%s%s~%s" % (upstream_version, ubuntu_version, branch_name,
+                                 vcs_name, vcs_revision, distro_version)
